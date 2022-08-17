71dbf272d9442bf0665b89665d7831e9c1e591e3
aymanASC22 <109638308+aymanASC22@users.noreply.github.com>
2022-08-17 11:33:51 -0400

    Add files via upload

---
+++ quiz1/questions.js
@@ -0,0 +1,75 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "What should not be included on your resume?",
+  answer: "Height",
+  options: [
+    "LinkedIn Account",
+    "Height",
+    "Email Address",
+    "Phone Number"
+  ]
+},
+  {
+  numb: 2,
+  question: "What are the 3 other sections on the resume?",
+  answer: "Experience, Education, Skills",
+  options: [
+    "Experience, Interests, Family",
+    "Education, Skills, Favorite Foods",
+    "Education, Friends, Experience",
+    "Experience, Education, Skills"
+  ]
+},
+  {
+  numb: 3,
+  question: "What should the education section include?",
+  answer: "Lowest level of education to highest level of education.",
+  options: [
+    "Lowest level of education to highest level of education",
+    "Past teachers",
+    "High school schedule",
+    "Past classmates."
+  ]
+},
+  {
+  numb: 4,
+  question: "Which of these are not included in the experience section?",
+  answer: "Homework Grades",
+  options: [
+    "Previous Employment",
+    "Internships",
+    "Volunteer Work",
+    "Homework Grades"
+  ]
+},
+  {
+  numb: 5,
+  question: "What should you not include in the Skills section?",
+  answer: "Diet/Appetite",
+  options: [
+    "Diet/Appetite",
+    "Languages You Speak",
+    "Skills in Microsoft Office",
+    "Public Presentation"
+  ]
+},
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
