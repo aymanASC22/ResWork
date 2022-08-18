5bda7c16aeebb535480bcf486338ffe7efc9a1a2
aymanASC22 <109638308+aymanASC22@users.noreply.github.com>
2022-08-18 10:06:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let gg = " IF YOU STOP reading NOW YOUR INSURANCE WILL RUN OUT, TRUE FACT" ;
+
+let button1 = document.getElementById("button1");
+
+
+document.getElementById("button1").onclick = function(){
+    document.getElementById("Intermission").innerHTML = gg;
+
+    console.log(gg);
+}
